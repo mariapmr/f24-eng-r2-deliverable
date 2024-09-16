da56d8be31bb3e96d2f79211bd9b58a0e53c05f4
CS61 User <nobody@example.com>
2024-09-16 03:04:04 -0400

    completed detailed species view, began functionality

---
+++ app/species/display-species-dialog.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import type { Database } from "@/lib/schema";
+import Image from "next/image";
+import { useState } from "react";
+// import sessionId from "./page";
+type Species = Database["public"]["Tables"]["species"]["Row"];
+// import EditSpeciesDialog from "./edit-species-dialog";
+
+export default function DisplaySpeciesDialog({ dispSpecies }: { dispSpecies: Species }) {
+  // Control open/closed state of the dialog
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className="mt-3 w-full">Learn More</Button>
+      </DialogTrigger>
+      <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+        <DialogHeader>
+          <DialogTitle>{dispSpecies.scientific_name}</DialogTitle>
+          <DialogDescription>{dispSpecies.common_name}</DialogDescription>
+        </DialogHeader>
+        {dispSpecies.image && (
+          <div className="relative h-40 w-full">
+            <Image src={dispSpecies.image} alt={dispSpecies.scientific_name} fill style={{ objectFit: "contain" }} />
+          </div>
+        )}
+        <p>Total population: {dispSpecies.total_population}</p>
+        <p>Kingdom: {dispSpecies.kingdom}</p>
+        <p>Description: {dispSpecies.description}</p>
+
+        {/* if(sessionId == dispSpecies.author) {
+          <EditSpeciesDialog />
+        } */}
+
+        <DialogClose asChild>
+          <Button type="button" className="ml-1 mr-1 flex-auto" variant="secondary">
+            Close
+          </Button>
+        </DialogClose>
+      </DialogContent>
+    </Dialog>
+  );
+}
